07e8552a48ff03379b114a5b9d5dfcb04f4cde7d
Aries Velasquez <velasquezaries11@gmail.com>
2023-02-20 19:46:16 +0800

    Implemented Authenticator

---
+++ lib/state/auth/backend/authenticator.dart
@@ -0,0 +1,86 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:instagram_clone/state/auth/constants/constants.dart';
+
+import '../../posts/typedef/user_id.dart';
+import '../models/auth_result.dart';
+
+class Authenticator {
+  User? get currentUser => FirebaseAuth.instance.currentUser;
+
+  UserId? get userId => currentUser?.uid;
+
+  bool get isAlreadyLogin => userId != null;
+
+  String get displayName => currentUser?.displayName ?? '';
+
+  String? get email => currentUser?.email;
+
+  Future<void> logOut() async {
+    await FirebaseAuth.instance.signOut();
+    await GoogleSignIn().signOut();
+    await FacebookAuth.instance.logOut();
+  }
+
+  Future<AuthResult> loginWithFaceBook() async {
+    final loginResult = await FacebookAuth.instance.login();
+    final token = loginResult.accessToken?.token;
+    if (token == null) {
+      return AuthResult.aborted;
+    }
+
+    final oAuthCredential = FacebookAuthProvider.credential(token);
+    try {
+      await FirebaseAuth.instance.signInWithCredential(
+        oAuthCredential,
+      );
+      return AuthResult.success;
+    } on FirebaseAuthException catch (e) {
+      final email = e.email;
+      final credential = e.credential;
+      if (e.code == Constants.accountExistsWithDifferentCredential &&
+          email != null &&
+          credential != null) {
+        final providers =
+            await FirebaseAuth.instance.fetchSignInMethodsForEmail(
+          email,
+        );
+        if (providers.contains(Constants.googleCom)) {
+          await loginWithGoogle();
+          FirebaseAuth.instance.currentUser?.linkWithCredential(
+            credential,
+          );
+          return AuthResult.success;
+        }
+      }
+      return AuthResult.failure;
+    }
+  }
+
+  Future<AuthResult> loginWithGoogle() async {
+    final GoogleSignIn googleSignIn = GoogleSignIn(scopes: [
+      Constants.emailScope,
+    ]);
+
+    final signInAccount = await googleSignIn.signIn();
+    if (signInAccount == null) {
+      return AuthResult.aborted;
+    }
+    final googleAuth = await signInAccount.authentication;
+    final oAuthCredentials = GoogleAuthProvider.credential(
+      idToken: googleAuth.idToken,
+      accessToken: googleAuth.accessToken,
+    );
+
+    try {
+      await FirebaseAuth.instance.signInWithCredential(
+        oAuthCredentials,
+      );
+      return AuthResult.success;
+    } catch (e) {
+      return AuthResult.failure;
+    }
+  }
+}
