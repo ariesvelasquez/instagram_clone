4848451cdcfc540f53af6de747a1ee7fbd73d421
Aries Velasquez <velasquezaries11@gmail.com>
2023-02-22 22:55:39 +0800

    Implemented User firestore creation and finalized auth state providers

--- lib/state/auth/notifiers/auth_state_notifiers.dart
+++ lib/state/auth/notifiers/auth_state_notifiers.dart
@@ -2,9 +2,13 @@ import 'package:instagram_clone/state/auth/backend/authenticator.dart';
 import 'package:instagram_clone/state/auth/models/auth_result.dart';
 import 'package:instagram_clone/state/auth/models/auth_state.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:instagram_clone/state/user_info/backend/user_info_storage.dart';
+
+import '../../posts/typedef/user_id.dart';
 
 class AuthStateNotifier extends StateNotifier<AuthState> {
   final _authenticator = const Authenticator();
+  final _userInfoStorage = const UserInfoStorage();
 
   AuthStateNotifier() : super(const AuthState.unknown()) {
     if (_authenticator.isAlreadyLogin) {
@@ -27,7 +31,39 @@ class AuthStateNotifier extends StateNotifier<AuthState> {
     final result = await _authenticator.loginWithGoogle();
     final userId = _authenticator.userId;
     if (result == AuthResult.success && userId != null) {
+      await saveUserInfo(
+        userId: userId,
+      );
 
+      state = AuthState(
+        result: result,
+        isLoading: false,
+        userId: userId,
+      );
     }
   }
+
+  Future<void> loginWithFacebook() async {
+    state = state.copiedWithIsLoading(true);
+    final result = await _authenticator.loginWithFaceBook();
+    final userId = _authenticator.userId;
+    if (result == AuthResult.success && userId != null) {
+      await saveUserInfo(
+        userId: userId,
+      );
+
+      state = AuthState(
+        result: result,
+        isLoading: false,
+        userId: userId,
+      );
+    }
+  }
+
+  Future<void> saveUserInfo({required UserId userId}) =>
+      _userInfoStorage.saveUserInfo(
+        userId: userId,
+        displayName: _authenticator.displayName,
+        email: _authenticator.email,
+      );
 }
---
+++ lib/state/auth/providers/user_id_provider.dart
@@ -0,0 +1,8 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:instagram_clone/state/auth/providers/auth_state_provider.dart';
+
+import '../../posts/typedef/user_id.dart';
+
+final userIdProvider = Provider<UserId?>(
+  (ref) => ref.watch(authStateProvider).userId,
+);
